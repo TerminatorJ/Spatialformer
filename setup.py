a8ba3d8487e3e974edf4851bcbc4ac24a1366d92
Jun Wang <35064020+TerminatorJ@users.noreply.github.com>
2025-02-19 14:38:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,5 +17,17 @@ setup(
     ],
     python_requires='>=3.9', 
     install_requires=requirements,
-    packages=find_packages(include=['spatialformer'])
+    packages=find_packages(include=['spatialformer']),
+    extras_require={
+        'amd': [
+            'torch==2.3.1+rocm6.0',  
+            'torchaudio==2.3.1+rocm6.0',
+            'torchvision==0.18.1+rocm6.0'
+        ],
+        'nvidia': [
+            'torch==2.3.1+cu121',
+            'torchvision==0.18.1+cu121',
+            'torchaudio==2.3.1+cu121',
+        ]
+    },
 )
